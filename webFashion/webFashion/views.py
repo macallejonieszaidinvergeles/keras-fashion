96fc0b6d4fd7edb1ce93970428863a0bdfb64226
Maxi <macallejon281@ieszaidinvergeles.org>
2022-04-21 17:52:25 +0200

    proyecto finalizado

--- webFashion/webFashion/views.py
+++ webFashion/webFashion/views.py
@@ -24,13 +24,10 @@ def infer_prec(img, img_size):
 
 
 def inicio(request):
-
-
     model_keras = keras.models.load_model("./my_model.h5")
     # print("model keras:",model_keras)
 
     if request.POST:
-
         upload = request.FILES['file']
         fss = FileSystemStorage()
         file = fss.save(upload.name, upload)
@@ -38,18 +35,35 @@ def inicio(request):
 
         # file = request.POST["file"]
         
-        # image_path = file_url
-        image_path = "chanclas-unisex-adidas-adilette-35543_jcFFoGe.jpg"
+        print("file:",file)
+        print("file_url:",file_url[1:])
+        image_path = file_url[1:]
         print("la ruta del archivo:",image_path)
 
-        # # GRIS
-        img = cv2.imread(image_path, 0)   # read image as gray scale    
-        print("img shape",img.shape)   # (300, 231)
+        img = cv2.imread(image_path, 0)  # read image as gray scale    
+        img = cv2.bitwise_not(img)             # < ----- bitwise_not
+        print(img.shape)   # (300, 231)
+
         img = infer_prec(img, 28)  # call preprocess function 
+        print(img.shape)   # (1, 28, 28, 1)
         y_pred = model_keras.predict(img)
         print(y_pred)
         prediction = tf.argmax(y_pred, axis=-1).numpy()
         print("la prediccion es:",prediction)
+        print("0: T-shirt/top 1: Trouser 2: Pullover 3: Dress 4: Coat 5: Sandal 6: Shirt 7: Sneaker 8: Bag 9: \n Ankle boot /n0: camiseta/top 1: pantalón 2: jersey 3: vestido 4: abrigo 5: sandalia 6: camisa 7: tenis 8: bolso 9: botín")
+
+        dict_items = {0: "camiseta/top" ,1: "pantalón", 2: "jersey", 3: "vestido", 4: "abrigo", 5: "sandalia",
+         6: "camisa", 7: "tenis", 8: "bolso", 9: "botín"}
+
+        prediction = str(prediction)
+        prediction = int(prediction[1:2])
+
+        for item in dict_items:
+            if prediction == item:
+                result = dict_items[item]
+        print("result:",result)
+     
+        return render(request, "inicio.html",{"prediction":result,'file_url': file_url[1:]})
         
 
     return render(request, "inicio.html")
